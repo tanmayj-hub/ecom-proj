6dfc2420e5e79ca73060ccbf3b6475e983dd55d3
Tanmay J <tanmay.work96#outlook.com>
2025-03-11 01:10:12 -0400

    Testing CI/CD after fixing ECR issue

--- microservices/product-service/src/app.ts
+++ microservices/product-service/src/app.ts
@@ -18,4 +18,4 @@ app.listen(PORT, () => {
   console.log(`Product-service running clearly at port ${PORT}`);
 });
 
-// change made to check if git push works and cicd pipeline works second time 
+// change made to check if git push works and cicd pipeline works third time 
